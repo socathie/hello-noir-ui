8042d74150780aa2fff7754707820b4e2b4a660f
Cathie So <socathie@gmail.com>
2022-12-18 02:20:17 +0800

    create worker script to compute witnesses

--- src/generate_calldata.ts
+++ src/generate_calldata.ts
@@ -1,8 +1,7 @@
-import { create_proof } from '@noir-lang/barretenberg/dest/client_proofs';
-import setup_generic_prover from "./setup_generic_prover";
 import initAztec from "@noir-lang/aztec_backend";
 import initNoir from '@noir-lang/noir_wasm';
 import { acir_from_bytes } from '@noir-lang/noir_wasm';
+import setup_generic_prover from "./setup_generic_prover";
 
 export interface abi {
     x: number,
@@ -16,17 +15,32 @@ export async function generateCalldata(input: abi) {
     let response = await fetch('main.buf');
     let buffer = await response.arrayBuffer();
     const circuit = new Uint8Array(buffer);
-    
-    let [prover, ] = await setup_generic_prover(circuit);
 
     response = await fetch('acir.buf');
     buffer = await response.arrayBuffer();
     const bytes = new Uint8Array(buffer);
     const acir = acir_from_bytes(bytes);
-
-    console.log(acir);
     
-    const proof = await create_proof(prover, acir, input);
+    let [prover, ] = await setup_generic_prover(circuit);
+    
+    const worker = new Worker(new URL('./worker.js', import.meta.url));
+
+    const errorPromise = new Promise((resolve, reject) => {
+        worker.onerror = (e) => {
+            reject(e);
+        }
+    });
+
+    const resultPromise = new Promise((resolve, reject) => {
+        worker.onmessage = (e) => {
+            resolve(e.data);
+        }
+    });
+
+    worker.postMessage({ url: document.URL, acir, input });
+    
+    const witness: any = await Promise.race([errorPromise, resultPromise]);
+    const proof = await prover.createProof(witness);
 
     return proof;
 }
